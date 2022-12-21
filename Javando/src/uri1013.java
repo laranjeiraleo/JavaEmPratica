aa3e1bca3817396de4b7793af5659658ad1f1762
laranjeiraleo <leo_laranjeira@hotmail.com>
2022-12-20 21:37:35 -0300

    Consumo

--- Javando/src/uri1013.java
+++ Javando/src/uri1013.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 public class uri1013 {
 
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		
-		int A = sc.nextInt();	
-		int B = sc.nextInt();	
-		int C = sc.nextInt();	
-		
-		int maiorAB = (A + B + Math.abs(A-B)) / 2;
-		int maiorABC = (maiorAB + C + Math.abs(maiorAB - C)) / 2;
-		
-		System.out.println(maiorABC + " eh o maior");
-		
-		
-		sc.close();
+	Locale.setDefault(Locale.US);
+	Scanner sc = new Scanner(System.in);
 	
+	int X = sc.nextInt();
+	double Y = sc.nextDouble();
+	
+	double ConsumoMedio = X / Y;
+	
+	System.out.printf("%.3f km/l%n", ConsumoMedio);
+	
+	
+	
+	
+	
+	sc.close();
 		
 
 	}
