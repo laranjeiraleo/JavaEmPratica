f02e8fe88356e9abd8f237df7ae3e203eeff0526
laranjeiraleo <leo_laranjeira@hotmail.com>
2022-12-20 21:45:32 -0300

    Consumo

---
+++ Javando/src/uri1014.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class uri1014 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int X = sc.nextInt();
+		double Y = sc.nextDouble();
+		
+		double ConsumoMedio = X / Y; 
+		
+		System.out.printf("%.3f km/l%n", ConsumoMedio);
+		
+		
+		
+		
+		
+		sc.close();
+		
+
+	}
+
+}
