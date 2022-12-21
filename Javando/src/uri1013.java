f6820652f2865499c3345f439eb2405b2883099e
laranjeiraleo <leo_laranjeira@hotmail.com>
2022-12-20 21:42:37 -0300

    O Maior

--- Javando/src/uri1013.java
+++ Javando/src/uri1013.java
@@ -7,15 +7,14 @@ public class uri1013 {
 	Locale.setDefault(Locale.US);
 	Scanner sc = new Scanner(System.in);
 	
-	int X = sc.nextInt();
-	double Y = sc.nextDouble();
+	int A = sc.nextInt();
+	int B = sc.nextInt();
+	int C = sc.nextInt();
 	
-	double ConsumoMedio = X / Y;
+	int MaiorAB = (A+B + Math.abs(A-B)) / 2;
+	int MaiorABC = (MaiorAB + C + Math.abs(MaiorAB - C)) / 2;
 	
-	System.out.printf("%.3f km/l%n", ConsumoMedio);
-	
-	
-	
+	System.out.println(MaiorABC + " eh o maior");
 	
 	
 	sc.close();
