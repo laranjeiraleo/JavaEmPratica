80ce51393f04a062347b2aa2f516b5bb8f62b8a4
laranjeiraleo <leo_laranjeira@hotmail.com>
2022-12-20 22:27:06 -0300

    Distância

---
+++ Javando/src/uri1016.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class uri1016 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int Distancia = sc.nextInt();
+		
+		int Tempo = Distancia * 2;
+		
+		System.out.println(Tempo + " minutos");
+		
+		
+		sc.close();
+
+	}
+
+}
